5868e18bc52a715b28c1aac34a7b4d5c1e83eb3b
45dy09 <92959976+45dy09@users.noreply.github.com>
2021-11-18 20:35:36 -0600

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,8 @@
+class Log extends Padre{
+    constructor(x, y, height, angle) {
+      super(x,y,20,height,angle);
+     this.image=loadImage("sprite/wood2.png");
+     Matter.Body.setAngle(this.body,angle);
+      }
+    }
+    
